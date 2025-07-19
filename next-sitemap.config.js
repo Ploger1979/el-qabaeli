744926f9ffc678e6968fb4e8ef1823ce1755d55e
Ayman Plöger <a.ploeger@enodia.de>
2025-07-19 13:50:14 +0200

    Initial commit: el-qabaeli-site export ready

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://el-qabaeli.netlify.app",
+  generateRobotsTxt: true,
+  sitemapSize: 5000, // optional
+  changefreq: "weekly",
+  priority: 0.7,
+};
